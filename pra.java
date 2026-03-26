bd0896d41673b25c79a957dd8ee816e2cf8283b9
gauridipakbansode.gmail.com <gauridipakbansode@gmail.com>
2026-03-26 11:32:11 +0530

    change in pra.java

--- pra.java
+++ pra.java
@@ -1,5 +1,5 @@
 public class pra {
     public static void main(String[] args){
-        System.out.println("This is practice for devOps T1");
+        System.out.println("This is practice for T1");
     }
 }
