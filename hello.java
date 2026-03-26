859c442ab864a8cf121bcdd53fc1adfbf314e814
gauridipakbansode.gmail.com <gauridipakbansode@gmail.com>
2026-03-26 10:46:54 +0530

    change

--- hello.java
+++ hello.java
@@ -2,15 +2,11 @@ import java.util.*;
 
 class hello{
     public static void main(String[] args){
-<<<<<<< HEAD
 
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter your name: ");
         String name = sc.nextLine();
 
         System.out.println("Hello world, This is " + name);
-=======
-        System.out.println("Hello world i am gauri");
->>>>>>> f545fde3224e474a13c866074dc45338ae56817f
     }
 }
