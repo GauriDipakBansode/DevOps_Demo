90f4a48ff66ebfff6c3b6944e5adc5ad36e2668c
gauridipakbansode.gmail.com <gauridipakbansode@gmail.com>
2026-03-26 10:42:37 +0530

    chainging file

--- hello.java
+++ hello.java
@@ -1,5 +1,12 @@
+import java.util.*;
+
 class hello{
     public static void main(String[] args){
-        System.out.println("Hello world");
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = sc.nextLine();
+
+        System.out.println("Hello world, This is " + name);
     }
 }
